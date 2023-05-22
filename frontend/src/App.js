6c2aab8882cbb7f67cf9ce965f50f2a92d6271f4
Koji Miyata <calcio.cogi@gmail.com>
2023-05-22 23:53:37 +0900

    update front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [gameStarted, setGameStarted] = useState(false);
   const [message, setMessage] = useState(null);
   const [worldState, setWorldState] = useState([]);
   const [size, setSize] = useState(10);
@@ -13,121 +14,134 @@ function App() {
     { name: "TestB", x: 10, y: 10, stamina: 10, wealth: 10 },
   ]);
 
-  const startGame = async () => {
-    try {
-      const response = await axios.post("http://localhost:9000/start_game", {
+  const startGame = () => {
+    axios
+      .post("/start_game_mock", {
         size,
         num_wealth: numWealth,
         agent_count: agentCount,
         agent_list: agents,
+      })
+      .then((response) => {
+        setMessage("Success!");
+        setTimeout(() => {
+          setMessage(null);
+        }, 10000);
+        setGameStarted(true);
+        // polling get world status api
+        const intervalId = setInterval(() => {
+          axios
+            .get("/get_world_state_mock")
+            .then((response) => {
+              if (response.data.length > 0) {
+                // check if the response data is not empty
+                setWorldState(response.data);
+              }
+            })
+            .catch((err) => console.error(err));
+        }, 2000);
+        return () => clearInterval(intervalId);
+      })
+      .catch((error) => {
+        console.error("Failed to call API", error);
       });
-
-      if (response.status === 200) {
-        setMessage("success");
-        setTimeout(() => setMessage(null), 10000);
-      }
-    } catch (error) {
-      console.log(error);
-    }
   };
-
-  const getWorldState = async () => {
-    try {
-      const response = await axios.get("http://localhost:9000/get_world_state");
-
-      if (response.status === 200) {
-        setWorldState(response.data);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    const intervalId = setInterval(getWorldState, 2000);
-
-    return () => clearInterval(intervalId);
-  }, []);
 
   return (
     <div className="app">
-      <form onSubmit={(e) => e.preventDefault()}>
-        <label>
-          Size:
-          <input
-            type="number"
-            value={size}
-            onChange={(e) => setSize(+e.target.value)}
-          />
-        </label>
-        <label>
-          Number of Wealth:
-          <input
-            type="number"
-            value={numWealth}
-            onChange={(e) => setNumWealth(+e.target.value)}
-          />
-        </label>
-        <label>
-          Number of Agents:
-          <input
-            type="number"
-            value={agentCount}
-            onChange={(e) => setAgentCount(+e.target.value)}
-          />
-        </label>
-        {agents.map((agent, i) => (
-          <div key={i}>
-            <label>
-              Agent Name:
-              <input
-                type="text"
-                value={agent.name}
-                onChange={(e) => {
+      {gameStarted ? null : (
+        <form className="form" onSubmit={(e) => e.preventDefault()}>
+          <label>
+            Size:
+            <input
+              type="number"
+              value={size}
+              onChange={(e) => setSize(+e.target.value)}
+            />
+          </label>
+          <label>
+            Number of Wealth:
+            <input
+              type="number"
+              value={numWealth}
+              onChange={(e) => setNumWealth(+e.target.value)}
+            />
+          </label>
+          <label>
+            Number of Agents:
+            <input
+              type="number"
+              value={agentCount}
+              onChange={(e) => setAgentCount(+e.target.value)}
+            />
+          </label>
+          {agents.map((agent, i) => (
+            <div key={i}>
+              <label>
+                Agent Name:
+                <input
+                  type="text"
+                  value={agent.name}
+                  onChange={(e) => {
+                    const newAgents = [...agents];
+                    newAgents[i].name = e.target.value;
+                    setAgents(newAgents);
+                  }}
+                />
+              </label>
+              <button
+                onClick={() => {
                   const newAgents = [...agents];
-                  newAgents[i].name = e.target.value;
+                  newAgents.splice(i, 1);
                   setAgents(newAgents);
                 }}
-              />
-            </label>
-            <button
-              onClick={() => {
-                const newAgents = [...agents];
-                newAgents.splice(i, 1);
-                setAgents(newAgents);
-              }}
-            >
-              Remove Agent
-            </button>
-          </div>
-        ))}
-        <button
-          onClick={() =>
-            setAgents([
-              ...agents,
-              { name: "", x: 0, y: 0, stamina: 0, wealth: 0 },
-            ])
-          }
-        >
-          Add Agent
-        </button>
-        <button onClick={startGame}>Start Game</button>
-      </form>
+              >
+                Remove Agent
+              </button>
+            </div>
+          ))}
+          <button
+            onClick={() =>
+              setAgents([
+                ...agents,
+                { name: "", x: 0, y: 0, stamina: 0, wealth: 0 },
+              ])
+            }
+          >
+            Add Agent
+          </button>
+          <button onClick={startGame}>Start Game</button>
+        </form>
+      )}
 
       {/* Success message */}
       {message && <div>{message}</div>}
 
       {/* World state */}
-      {worldState.map((row, i) => (
-        <div key={i}>
-          {row.map((cell, j) => (
-            <div key={j}>
-              {cell.cellValue === "W" && <div>💰</div>}
-              {cell.agentValue && <div>{cell.agentValue}</div>}
-            </div>
-          ))}
+      {gameStarted ? (
+        <div className="container">
+          <div className="grid-container">
+            {worldState.length > 0 ? (
+              worldState.map((row, i) => (
+                <div key={`row-${i}`} className="grid-row">
+                  {row.map((cell, j) => (
+                    <div key={`cell-${i}-${j}`} className="grid-cell">
+                      {cell === "W" ? <span className="coin">💰</span> : ""}
+                      {cell !== "W" && cell !== "null" ? (
+                        <span className="agent">{cell}</span>
+                      ) : (
+                        <span className="null">{}</span>
+                      )}
+                    </div>
+                  ))}
+                </div>
+              ))
+            ) : (
+              <p>Loading...</p>
+            )}
+          </div>
         </div>
-      ))}
+      ) : null}
     </div>
   );
 }
